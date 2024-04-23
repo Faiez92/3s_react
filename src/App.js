508f08d7939acf23282e1475418050045976b098
faiez <faiez162@gmail.com>
2024-04-23 15:02:52 +0200

    update front

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';  
+import BeneficiaryList from './components/BeneficiaryList';
+import CreateBeneficiary from './components/CreateBeneficiary';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BeneficiaryList />
+      <hr />
+      <CreateBeneficiary />
     </div>
   );
 }
